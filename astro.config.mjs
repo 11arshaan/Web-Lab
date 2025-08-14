e566f9dc846b2c131ebf263f20e2404850127d31
Harshaan <11arshaan@gmail.com>
2025-08-14 10:56:26 -0400

    refresh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      resolve: {
+        alias: {
+          '@components': path.resolve('./src/components'),
+          '@layouts': path.resolve('./src/layouts'),
+          '@graphics': path.resolve('./src/assets/Graphics'),
+          '@assets': path.resolve('./src/assets'),
+          // Add more as needed
+        }
+      }
+    },
+
+  integrations: [react()]
+});
--- src/data/experiments.ts
+++ src/data/experiments.ts
@@ -5,17 +5,38 @@ interface Experiment {
     slug: string,
     title: string,
     description: string,
-    image: ImageMetadata,
+    image?: ImageMetadata,
     tags?: string[]
 
 }
 
 export const experiments: Experiment[] = [
     {
-        slug: "Example1",
-        title: "Example1",
+        slug: "Example/example",
+        title: "Example 1",
         description: "An example item's description",
         image: previews.example,
-        tags: ["example", "utility"]
+        tags: ["example"]
+    },
+    {
+        slug: "Example/example",
+        title: "Example 2",
+        description: "This is the second example. It has a utility tag and a large description. Check text wrapping and general font measurements here.",
+        image: previews.example,
+        tags: ["utility"]
+    },
+    {
+        slug: "Example/example",
+        title: "Example 3",
+        description: "Another example. Has utility tag.",
+        image: previews.example,
+        tags: ["utility"]
+    },
+    {
+        slug: "Transitions/transition1",
+        title: "transition 1",
+        description: "preview transition.",
+        image: previews.example,
+        tags: ["transition"]
     }
 ]
